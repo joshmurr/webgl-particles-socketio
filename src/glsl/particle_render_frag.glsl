a60f2568a2b8484c7b9c878120fe0ce87906f511
Josh Murr <joshuadavidmurr@gmail.com>
2020-05-10 19:53:07 +0100

    Round points using gl_PointCoord

--- src/glsl/particle_render_frag.glsl
+++ src/glsl/particle_render_frag.glsl
@@ -12,6 +12,10 @@ vec3 palette(in float t, in vec3 a, in vec3 b, in vec3 c, in vec3 d){
 
 void main() {
     float t = v_Age/v_Life;
+    float distance = length(2.0 * gl_PointCoord - 1.0);
+    if (distance > 1.0) {
+            discard;
+    }
     o_FragColor = vec4(
             palette(t,
                 vec3(0.8,0.3,0.2),
